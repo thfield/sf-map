bc0caf6d285977e9a4013798639a474bc4041bf1
thfield <thfield@gmail.com>
2016-09-09 10:41:06 -0700

    add some example datasets

---
+++ script/data.js
@@ -0,0 +1,30 @@
+// contains ES6 code
+"use strict";
+const fs = require('fs')
+const json2csv = require('json2csv')
+let path = '../data/'
+
+let file = 'censustract.json'
+foo(path + file)
+
+function foo (file) {
+  let inputFile = '' + file
+  let outputFile = '' + file.replace('.json', '') + '.csv'
+
+  let originalText = fs.readFileSync(inputFile, 'utf8')
+  let processedText = JSON.parse(originalText)
+
+  processedText = processedText.data.map((el)=>{
+    return {tract:el.tract, population: el.population}
+  })
+
+  processedText = json2csv({data: processedText, fields: ['tract','population']})
+
+  // output the file
+  fs.writeFile(outputFile, processedText,
+    function(err) {
+      if (err) { return console.log(err); }
+      console.log("The file was saved as", outputFile);
+    }
+  )
+}
